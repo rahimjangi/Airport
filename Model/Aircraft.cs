3159e42fb58c77f2919d9dc71fe05b141fab9644
pc <rahimjangi1982@gmail.com>
2023-07-12 20:26:41 +0200

    Airline tables added!

---
+++ Model/Aircraft.cs
@@ -0,0 +1,8 @@
+﻿namespace Airport.Model;
+
+public class Aircraft
+{
+    public int AircraftId { get; set; }
+    public string Type { get; set; }
+    public int SeatingCapacity { get; set; }
+}
---
+++ Model/PricingAndAvailability.cs
@@ -0,0 +1,11 @@
+﻿namespace Airport.Model
+{
+    public class PricingAndAvailability
+    {
+        public int PricingId { get; set; }
+        public int FlightId { get; set; }
+        public DateTime Date { get; set; }
+        public int AvailableSeats { get; set; }
+        public decimal TicketPrice { get; set; }
+    }
+}
