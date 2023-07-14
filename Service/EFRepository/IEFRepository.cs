ff5fa76d763931bf65c87c457e465319acc20221
Houshang Kheiri <h.kh.sadigh@gmail.com>
2023-07-14 22:14:43 +0200

    Response class and IEFRepository class Added

---
+++ Service/EFRepository/IEFRepository.cs
@@ -0,0 +1,14 @@
+﻿namespace Airport.Service.EFRepository
+{
+    public interface IEFRepository<T>
+    {
+        Task<List<T>> GetAll();
+        Task<T> GetById(int id);
+        Task<T> GetByName(string name);
+        Task<T> Update(int id, T entity);
+        Task<T> UpdateByEntity(T entity);
+        Task DeleteById(int id);
+        Task DeleteByName(string name);
+        Task DeleteRange();      
+    }
+}
