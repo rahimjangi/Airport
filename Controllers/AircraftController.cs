dc8af81d19810a15b58bfde8be774f4bf62364e6
Houshang Kheiri <h.kh.sadigh@gmail.com>
2023-07-18 23:00:05 +0200

    AircraftController added

--- Controllers/AircraftController.cs
+++ Controllers/AircraftController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Airport.Dto;
+using Airport.Model;
+using Airport.Service;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Airport.Controllers
@@ -7,5 +10,34 @@ namespace Airport.Controllers
     [ApiController]
     public class AircraftController : ControllerBase
     {
+        private readonly IUnitOfWorkRepository _unitOfWork;
+
+        public AircraftController(IUnitOfWorkRepository unitOfWork)
+        {
+            this._unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ServiceResponse<Aircraft>>>> GetAll()
+        {
+            var result = await _unitOfWork.Aircraft.GetAll();
+            return Ok(result);
+        }
+
+        [HttpGet("/name")]
+        public async Task<ActionResult<ServiceResponse<Aircraft>>> GetByName(string aircraft)
+        {
+            var result = await _unitOfWork.Aircraft.GetByName(aircraft);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse<Aircraft>>> Add(AircraftDto aircraft)
+        {
+            var sr = new ServiceResponse<AircraftDto>();
+            sr.Data = aircraft;
+            await _unitOfWork.Aircraft.CreateByEntity(sr);
+            return Ok();
+        }
     }
 }
--- Service/AircraftService/AircraftRepository.cs
+++ Service/AircraftService/AircraftRepository.cs
@@ -1,6 +1,9 @@
 ﻿using Airport.Data;
 using Airport.Dto;
+using Airport.Model;
 using AutoMapper;
+using Azure;
+using Microsoft.EntityFrameworkCore;
 
 namespace Airport.Service.AircraftService
 {
@@ -14,9 +17,86 @@ namespace Airport.Service.AircraftService
             _context = context;
             _mapper = mapper;
         }
-        public Task CreateByEntity(ServiceResponse<AircraftDto> entity)
+
+        public async Task<List<ServiceResponse<AircraftDto>>> GetAll()
         {
-            throw new NotImplementedException();
+            var resultList = await _context.Aircrafts.ToListAsync();
+            var responseList = new List<ServiceResponse<AircraftDto>>();
+
+            if (resultList.Count > 0 && resultList != null) 
+            {
+               var mapResult = _mapper.Map<List<AircraftDto>>(resultList);
+
+                foreach(var result in mapResult)
+                {
+                    var response = new ServiceResponse<AircraftDto>
+                    {
+                        Data = result,
+                        Success = true,
+                        Message = "Aircraft exists."
+                    };
+
+                    responseList.Add(response);
+                }
+            }
+            else
+            {
+                var response = new ServiceResponse<AircraftDto> 
+                {
+                    Data = null,
+                    Success = false,
+                    Message = "No airlines found."
+                };
+
+                responseList.Add(response);
+            }
+
+            return responseList;
+        }
+
+        public async Task<ServiceResponse<AircraftDto>> GetByName(string name)
+        {
+            ServiceResponse <AircraftDto> response = new ServiceResponse<AircraftDto>();
+
+            var result = await _context.Aircrafts.FirstOrDefaultAsync(a => a.AircraftCode.Equals(name));
+
+            if(result != null) 
+            {
+                var mapResult = _mapper.Map<AircraftDto>(result);
+                response = new ServiceResponse<AircraftDto>
+                {
+                    Data = mapResult,
+                    Success = true,
+                    Message = "Airline found."
+                };
+                return response;
+            }
+            else
+            {
+                response = new ServiceResponse<AircraftDto>
+                {
+                    Data = null,
+                    Success = false,
+                    Message = "Airline not found."
+                };
+
+                return response;
+            }
+        }
+
+        public async Task CreateByEntity(ServiceResponse<AircraftDto> entity)
+        {
+            if(entity.Data != null) 
+            {
+               var mapResult = _mapper.Map<Aircraft>(entity.Data);
+               
+                await _context.Aircrafts.AddAsync(mapResult);
+                await _context.SaveChangesAsync();
+            }
+            else
+            {
+                await Task.CompletedTask;
+            }
         }
 
         public  Task DeleteById(int id)
@@ -33,22 +113,12 @@ namespace Airport.Service.AircraftService
         public Task DeleteRange()
         {
             throw new NotImplementedException();
-        }
-
-        public Task<List<ServiceResponse<AircraftDto>>> GetAll()
-        {
-            throw new NotImplementedException();
-        }
+        }       
 
         public Task<ServiceResponse<AircraftDto>> GetById(int id)
         {
             throw new NotImplementedException();
-        }
-
-        public Task<ServiceResponse<AircraftDto>> GetByName(string name)
-        {
-            throw new NotImplementedException();
-        }
+        }       
 
         public Task<ServiceResponse<AircraftDto>> Update(int id, ServiceResponse<AircraftDto> entity)
         {
